582c3c73b5275ac20ceeebb9ccc318a332e78a73
a1a2a3a4a5 <linub7@gmail.com>
2022-10-11 11:26:19 +0330

    web scraping & make a card preview

---
+++ components/post/PreviewCard.js
@@ -0,0 +1,56 @@
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import Text from '@kaloraat/react-native-text';
+import { Image } from 'react-native';
+
+const PreviewCard = ({
+  ogTitle = 'Untitled',
+  ogDescription = 'No Description Found',
+  ogImage = { url: 'https://via.placeholder.com/500x500.png?text=Image' },
+}) => {
+  console.log({ ogImage });
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: ogImage?.url }} style={styles.image} />
+
+      <TouchableOpacity>
+        <View style={styles.textContainer}>
+          <Text medium styles={styles.title}>
+            {ogTitle}
+          </Text>
+          <Text semi>{ogDescription}</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    height: 280,
+    width: '92%',
+    borderRadius: 14,
+    shadowColor: '#171717',
+    shadowOffset: {
+      width: -2,
+      height: 4,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    marginBottom: 20,
+    overflow: 'hidden',
+  },
+  image: {
+    height: '70%',
+    width: '100%',
+  },
+  textContainer: {
+    padding: 5,
+    height: 50,
+  },
+  title: {
+    paddingVertical: 5,
+  },
+});
+
+export default PreviewCard;
--- screens/Post.js
+++ screens/Post.js
@@ -1,14 +1,103 @@
-import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
 import Text from '@kaloraat/react-native-text';
+import urlRegex from 'url-regex';
+import ogs from '@uehreka/open-graph-scraper-react-native';
+
 import FooterTabs from '../components/nav/footer/FooterTabs';
+import SubmitButton from '../components/auth/SubmitButton';
+import PreviewCard from '../components/post/PreviewCard';
 
 const Post = () => {
+  const [values, setValues] = useState({
+    link: '',
+    title: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const [urlPreview, setUrlPreview] = useState({});
+
+  const handleChange = async (text) => {
+    try {
+      setLoading(true);
+      setValues((currentValues) => {
+        return {
+          ...currentValues,
+          link: text,
+        };
+      });
+
+      if (urlRegex({ strict: false }).test(text)) {
+        ogs({ url: text }, (error, results, response) => {
+          console.log(results);
+          if (results.success) {
+            setUrlPreview(results);
+          }
+          setLoading(false);
+        });
+      } else {
+        setLoading(false);
+      }
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = () => {
+    console.log({ values });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <Text>Post</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Text light center style={styles.text}>
+          Past Website url
+        </Text>
+        <TextInput
+          value={values?.link}
+          onChangeText={(value) => handleChange(value)}
+          placeholder="Paste the URL"
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.input}
+        />
+        <TextInput
+          value={values?.title}
+          onChangeText={(value) =>
+            setValues((currentValues) => {
+              return {
+                ...currentValues,
+                title: value,
+              };
+            })
+          }
+          placeholder="Enter a title"
+          autoCapitalize="none"
+          style={styles.input}
+        />
+
+        {urlPreview.success && (
+          <View style={styles.previewCardContainer}>
+            <PreviewCard {...urlPreview} />
+          </View>
+        )}
+
+        {/* <Text>{JSON.stringify(urlPreview, null, 4)}</Text> */}
+
+        <SubmitButton
+          label={'Submit'}
+          loading={loading}
+          onPress={handleSubmit}
+          additionalStyle={styles.submitButton}
+        />
       </ScrollView>
-      <View style={styles.footerContainer}>
+      <View>
         <FooterTabs />
       </View>
     </SafeAreaView>
@@ -18,6 +107,26 @@ const Post = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  text: {
+    paddingTop: 30,
+    marginBottom: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    height: 50,
+    marginVertical: 10,
+    marginHorizontal: 15,
+    borderRadius: 30,
+    padding: 15,
+  },
+  submitButton: {
+    marginTop: 20,
+  },
+  previewCardContainer: {
+    marginTop: 30,
+    alignItems: 'center',
   },
 });
 
